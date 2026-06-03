6927cd1f2f01e921857daa297635c5b2d2cd74fa
Danila <dan.reg.reca@gmail.com>
2026-06-03 15:56:46 -0300

    agregue paginas Accesorios y comida, y estilos de paginacion

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -3,5 +3,7 @@ import homeController from "../controllers/homeController.js";
 
 const router = express.Router();
 router.get("/productos", homeController.productos);
+router.get("/productos/comida", homeController.comida);
+router.get("/productos/accesorios", homeController.accesorios);
 
 export default router;
