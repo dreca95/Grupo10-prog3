0e68044f9a88a602dadcd6151a54f3db71209f73
Danila <dan.reg.reca@gmail.com>
2026-06-19 17:32:38 -0300

    agregar boton autocompletado

---
+++ public/js/loginAutocompletado.js
@@ -0,0 +1,4 @@
+document.getElementById("autocompletadoBtn").addEventListener("click", () => {
+    document.getElementById("usuario").value = "admin";
+    document.getElementById("password").value = "1234";
+});
